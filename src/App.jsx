ec40807e8ab76a727d041fc50200db4f41f121f4
gdrmvrm <gdrmvrm@gmail.com>
2025-08-24 13:04:11 +0100

    Add header with logo and section with hero image and text

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 
 const App = () => {
   return (
-    <>
-      <h1 className="text-3xl font-mono text-center mt-2">CineTalkies</h1>
-    </>
+    <main>
+      <header className="flex flex-row items-center w-full justify-center mt-5 mb-5">
+        <img src="./logo.png" alt="black & white reel logo" width={50} className="mt-5 w-10 sm:w-20 md:w-[50px]" />
+        <h1 className="text-2xl sm:text-3xl font-mono text-center mt-10">
+          Cine<span className="text-red-700">Talkies</span>
+        </h1>
+      </header>
+
+      <section className="flex flex-col items-center justify-center">
+        <img src="./hero.png" alt="hero banner" className="w-90 sm:w-200 md:w-[480px] mb-10" />
+        <h1 className="text-2xl sm:text-3xl font-mono text-center">
+          Smart Picks. <span className="text-red-700"> Zero Fuss.</span>
+        </h1>
+      </section>
+    </main>
   );
 };
 
